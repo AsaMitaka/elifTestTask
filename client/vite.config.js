2ef6929e73d3c50413a9029b95a21fa31f4afb99
AsaMitaka <cepelits@gmail.com>
2023-05-28 21:07:46 +0300

    client fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'css',
+      modules: true, // Enable CSS modules
+    },
+  ],
   base: '/elifTestTask/',
 });
